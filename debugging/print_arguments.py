03e86f4386a808ecb0ff7f4c8e47f3ac4ea62ef7
Djelloul rouba <djelloulrouba@yahoo.com>
2025-04-17 09:52:39 +0200

    Script update fix problem arg with chat gpt

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-def main():
-    """
-    Point d'entrée du script.
-
-    Vérifie les arguments de ligne de commande et les affiche.
-    """
-    if len(sys.argv) > 1:
-        print("Arguments :")
-        for i in range(1, len(sys.argv)):
-            print(sys.argv[i])
-    else:
-        print("Aucun argument fourni.")
-
-if __name__ == "__main__":
-    main()
+for arg in sys.argv[1:]:
+    print(arg)
